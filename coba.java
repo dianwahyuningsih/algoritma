8505080cca08721f0e37fc2aa734177890063254
dianwahyuningsih <106479449+dianwahyuningsih@users.noreply.github.com>
2022-09-13 13:15:35 +0700

    Create coba.java

---
+++ coba.java
@@ -0,0 +1,12 @@
+public class ProjectPertama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Percobaan Pertama");
+        System.out.println("Nama Saya Dian");
+    }
+    
+}
